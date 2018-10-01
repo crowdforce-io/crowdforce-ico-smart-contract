34d2d2566ecde6a598abcf3dcff042577c60a931
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2018-10-01 22:49:01 +0100

    Added balance checking and transfer fee processing

--- CRDtransfer.sol
+++ CRDtransfer.sol
@@ -1,24 +1,23 @@
 pragma solidity ^0.4.0;
 
 contract CRDTransferTo {
-    function() public payable {
-        
-    }
+    function() public payable {}
     
-    function getBalance() public returns (uint) {
+    function getBalance() view returns (uint256) {
         return address(this).balance;
     }
 }
 
 contract CRDTransferFrom {
-    
+    address crd_wallet;
+    uint amount;
     CRDTransferTo private _instance;
     
-    constructor() {
+    constructor() public {
         _instance = new CRDTransferTo();   
     }
     
-    function getBalance() public returns (uint) {
+    function getBalance() private returns (uint256) {
         return address(this).balance;
     }
     
@@ -27,6 +26,12 @@ contract CRDTransferFrom {
     }
     
     function () public payable {
-        address(_instance).send(msg.value);
+        uint fee = (amount * 5)/100;
+        if(getBalance() > fee) {
+            crd_wallet.transfer(fee);
+            if (getBalance() > amount) {
+                address(_instance).transfer(amount);      
+            }
+        }
     }
 }
